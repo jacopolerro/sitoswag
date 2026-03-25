4b4c8987ad50af5243c6b88aa415eda6b1746935
Jacopo Lerro <jacopolerro@192.168.1.92>
2026-03-25 23:49:21 +0100

    Configure GitHub Pages deployment and clean up project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Questo rende il sito portatile e immune a errori di percorso
+  base: '/sitoswag/', // Essenziale per GitHub Pages!
 })
