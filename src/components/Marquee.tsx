3097984c709d97f573befb71eb44213a83c3981d
Jacopo Lerro <jacopolerro@192.168.1.92>
2026-04-10 23:18:29 +0200

    Network section updates and marquee alignment attempt

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,7 +1,7 @@
 export const Marquee = () => (
-  <div className="marquee border-top">
+  <div className="marquee">
     <div className="marquee-content">
-      FIVESEVEN CBR // STAY SWAG // SWAG PEOPLE MAKES SAFE SEX // TRULY HUMBLE UNDER GOD // NO CAP // STAY DRIPPY // FIVESEVEN CBR // STAY SWAG // SWAG PEOPLE MAKES SAFE SEX // TRULY HUMBLE UNDER GOD // NO CAP // STAY DRIPPY //
+      FIVESEVEN CBR320 // STAY SWAG // SWAG PEOPLE MAKES SAFE SEX // JUST CLUBBING // STAY DRIPPY // FIVESEVEN CBR320 // STAY SWAG // SWAG PEOPLE MAKES SAFE SEX // JUST CLUBBING // STAY DRIPPY //
     </div>
   </div>
 );
